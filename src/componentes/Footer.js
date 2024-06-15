b999c99ceef35e1464e07e90d0fdc395e5b34568
Matidmartinez <martinez.dmatias@gmail.com>
2024-06-15 18:14:13 -0400

    Se crea componente footer

---
+++ src/componentes/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ReactComponent as WhatsApp} from '../assets/whatsApp.svg'
+import '../styles/Footer.css';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-logo">
+        <img src={logo} alt="Logo de Terrasol" /> 
+      </div>
+      <div className="footer-address">
+       Av. América #670, San Bernardo, Santiago de Chile
+      </div>
+      <div className="footer-contact">
+        <a href="https://wa.me/56950186583" target="_blank" rel="noopener noreferrer">
+          <WhatsApp />
+        </a>
+        <p className="fab fa-whatsapp">+56 9 9758 4316</p>
+      </div>
+      <hr></hr>
+      <div className="footer-logos">
+        <a 
+          href="https://www.facebook.com/tuparcelaenelsurdechile?mibextid=ZbWKwL" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          aria-label="Facebook de Terrasol"
+        >
+          <img src={logofb} alt="Facebook" />
+        </a>
+        <a 
+          href="https://www.instagram.com/terrasol_parcelas/?igsh=eG51a2w4NGx5ODJu" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          aria-label="Instagram de Terrasol"
+        >
+          <img src={logoig} alt="Instagram" />
+        </a>
+      </div>
+    </footer>
+  );
+};
